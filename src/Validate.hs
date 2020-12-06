2430c61e54af840014cefcf9d9351c5e7e1af077
James Thomas <codeactor@protonmail.com>
2020-12-06 14:08:34 +0000

    Basic full body parser

--- src/Validate.hs
+++ src/Validate.hs
@@ -8,16 +8,36 @@ import qualified Text.Megaparsec.Char as M
 type Parser = Parsec Void String
 
 jsonParser :: Parser String
-jsonParser = emptyBodyParser
+jsonParser = emptyBodyParser <|> fullBodyParser
 
 emptyBodyParser :: Parser String
-emptyBodyParser = do
+emptyBodyParser = M.try $ do
   start <- bodyStartParser
   end <- bodyEndParser
   pure [start, end]
+
+fullBodyParser :: Parser String
+fullBodyParser = M.try $ do
+  start <- bodyStartParser
+  contents <- keyValueParser
+  end <- bodyEndParser
+  pure $ [start] ++ contents ++ [end]
 
 bodyStartParser :: Parser Char
 bodyStartParser = M.char '{'
 
 bodyEndParser :: Parser Char
 bodyEndParser = M.char '}'
+
+keyValueParser :: Parser String
+keyValueParser = do
+  key <- keyParser
+  sep <- M.char ':'
+  value <- valueParser
+  pure $ key ++ [sep] ++ value
+
+keyParser :: Parser String
+keyParser = M.many M.alphaNumChar
+
+valueParser :: Parser String
+valueParser = M.many M.alphaNumChar
