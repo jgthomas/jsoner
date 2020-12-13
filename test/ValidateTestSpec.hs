1492653d3c920df76d813bd06360da2c4217e4ff
James Thomas <codeactor@protonmail.com>
2020-12-13 11:47:04 +0000

    Second test

--- test/ValidateTestSpec.hs
+++ test/ValidateTestSpec.hs
@@ -9,4 +9,9 @@ spec :: Spec
 spec = do
   describe "Valid JSON" $
     it "Should validate an empty JSON" $
-      M.parse jsonParser "" "{}" `shouldParse` "{}"
+      M.parse jsonParser "" "{}"
+        `shouldParse` "{}"
+  describe "Valid JSON" $
+    it "Should validate a JSON with single key-value pair" $
+      M.parse jsonParser "" "{\"key\":\"value\"}"
+        `shouldParse` "{\"key\":\"value\"}"
