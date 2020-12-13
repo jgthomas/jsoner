0a786511dbc5d6fd2ae9f05cf44deb34b583fe29
James Thomas <codeactor@protonmail.com>
2020-12-13 12:05:55 +0000

    Range of other value tests

--- test/ValidateTestSpec.hs
+++ test/ValidateTestSpec.hs
@@ -12,9 +12,25 @@ spec = do
       M.parse jsonParser "" "{}"
         `shouldParse` "{}"
   describe "Valid JSON" $
-    it "Should validate a JSON with single key-value pair" $
+    it "Should validate a JSON with single string key-value pair" $
       M.parse jsonParser "" "{\"key\":\"value\"}"
         `shouldParse` "{\"key\":\"value\"}"
+  describe "Valid JSON" $
+    it "Should validate a JSON with boolean TRUE value" $
+      M.parse jsonParser "" "{\"key\":true}"
+        `shouldParse` "{\"key\":true}"
+  describe "Valid JSON" $
+    it "Should validate a JSON with boolean FALSE value" $
+      M.parse jsonParser "" "{\"key\":false}"
+        `shouldParse` "{\"key\":false}"
+  describe "Valid JSON" $
+    it "Should validate a JSON with NULL value" $
+      M.parse jsonParser "" "{\"key\":null}"
+        `shouldParse` "{\"key\":null}"
+  describe "Valid JSON" $
+    it "Should validate a JSON with empty value" $
+      M.parse jsonParser "" "{\"key\":\"\"}"
+        `shouldParse` "{\"key\":\"\"}"
   describe "Invalid JSON" $
     it "Should fail validation with unquoted key" $
       M.parse jsonParser "{key:\"value\"}"
