47c501d6aa694dacfc562b74ccdd186426ac3884
James Thomas <codeactor@protonmail.com>
2020-12-31 16:26:42 +0000

    More tests for array values

--- test/ValidateArraySpec.hs
+++ test/ValidateArraySpec.hs
@@ -11,3 +11,23 @@ spec = do
     it "Should validate a JSON with an array of strings value" $
       M.parse jsonParser "" "{\"key\":[\"one\",\"two\"]}"
         `shouldParse` "{\"key\":[\"one\",\"two\"]}"
+  describe "Valid JSON" $
+    it "Should validate a JSON with an array of intergers value" $
+      M.parse jsonParser "" "{\"key\":[123,456]}"
+        `shouldParse` "{\"key\":[123,456]}"
+  describe "Valid JSON" $
+    it "Should validate a JSON with an array leading space" $
+      M.parse jsonParser "" "{\"key\":    [\"one\",\"two\"]}"
+        `shouldParse` "{\"key\":[\"one\",\"two\"]}"
+  describe "Valid JSON" $
+    it "Should validate a JSON with an array trailing space" $
+      M.parse jsonParser "" "{\"key\":[\"one\",\"two\"]    }"
+        `shouldParse` "{\"key\":[\"one\",\"two\"]}"
+  describe "Valid JSON" $
+    it "Should validate a JSON with an array leading and trailing space" $
+      M.parse jsonParser "" "{\"key\":    [\"one\",\"two\"]    }"
+        `shouldParse` "{\"key\":[\"one\",\"two\"]}"
+  describe "Invalid JSON" $
+    it "Should fail validation with missing comma" $
+      M.parse jsonParser ""
+        `shouldFailOn` "{\"key\":[\"one\"\"two\"]}"
