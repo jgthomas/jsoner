1952c38400686edacf59b03e8b1ed1f010641d31
James Thomas <codeactor@protonmail.com>
2021-01-01 12:49:16 +0000

    Tests for escaped slashes

--- test/ValidateStringSpec.hs
+++ test/ValidateStringSpec.hs
@@ -8,9 +8,17 @@ import ValidateString (stringParser)
 spec :: Spec
 spec = do
   describe "Valid JSON" $
-    it "Should validate a JSON with a string value" $
+    it "Should validate with a string value" $
       M.parse stringParser "" "\"value\""
         `shouldParse` "\"value\""
+  describe "Valid JSON" $
+    it "Should validate with an escaped forward slash" $
+      M.parse stringParser "" "\"val\\/ue\""
+        `shouldParse` "\"val\\/ue\""
+  describe "Valid JSON" $
+    it "Should validate with an escaped back slash" $
+      M.parse stringParser "" "\"val\\\\ue\""
+        `shouldParse` "\"val\\\\ue\""
   describe "Inalid JSON" $
     it "Should fail validation when missing opening quote" $
       M.parse stringParser ""
