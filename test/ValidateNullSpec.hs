b73b235aa3834662f1b6ca0198b02074a98f316c
James Thomas <codeactor@protonmail.com>
2021-01-02 11:28:39 +0000

    Format null tests

--- test/ValidateNullSpec.hs
+++ test/ValidateNullSpec.hs
@@ -11,22 +11,27 @@ spec = do
     it "Should validate a null value" $
       M.parse jsonParser "" "{\"key\":null}"
         `shouldParse` "{\"key\":null}"
+
   describe "Valid JSON" $
     it "Should validate a null value leading space" $
       M.parse jsonParser "" "{\"key\":  null}"
         `shouldParse` "{\"key\":null}"
+
   describe "Valid JSON" $
     it "Should validate a null value trailing space" $
       M.parse jsonParser "" "{\"key\":null  }"
         `shouldParse` "{\"key\":null}"
+
   describe "Valid JSON" $
     it "Should validate a null value leading and trailing space" $
       M.parse jsonParser "" "{\"key\":  null  }"
         `shouldParse` "{\"key\":null}"
+
   describe "Invalid JSON" $
     it "Should fail validation with mixed case" $
       M.parse jsonParser ""
         `shouldFailOn` "{\"key\":nUll}"
+
   describe "Invalid JSON" $
     it "Should fail validation with upper case" $
       M.parse jsonParser ""
