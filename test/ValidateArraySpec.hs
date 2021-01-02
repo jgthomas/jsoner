511182b8bcc13e26f466c94e2527d90518e4629d
James Thomas <codeactor@protonmail.com>
2021-01-02 11:27:21 +0000

    Fix string tests

--- test/ValidateArraySpec.hs
+++ test/ValidateArraySpec.hs
@@ -11,54 +11,67 @@ spec = do
     it "Should validate a JSON with an array of string values" $
       M.parse jsonParser "" "{\"key\":[\"one\",\"two\"]}"
         `shouldParse` "{\"key\":[\"one\",\"two\"]}"
+
   describe "Valid JSON" $
     it "Should validate a JSON with an array of interger values" $
       M.parse jsonParser "" "{\"key\":[123,456]}"
         `shouldParse` "{\"key\":[123,456]}"
+
   describe "Valid JSON" $
     it "Should validate a JSON with an array of empty objects" $
       M.parse jsonParser "" "{\"key\":[{},{}]}"
         `shouldParse` "{\"key\":[{},{}]}"
+
   describe "Valid JSON" $
     it "Should validate a JSON with an array of objects with fields" $
       M.parse jsonParser "" "{\"key\":[{\"key\":123},{\"key\":456}]}"
         `shouldParse` "{\"key\":[{\"key\":123},{\"key\":456}]}"
+
   describe "Valid JSON" $
     it "Should validate a JSON with an array of empty arrays" $
       M.parse jsonParser "" "{\"key\":[[],[]]}"
         `shouldParse` "{\"key\":[[],[]]}"
+
   describe "Valid JSON" $
     it "Should validate a JSON with an array of integer arrays" $
       M.parse jsonParser "" "{\"key\":[[1,2,3],[4,5,6]]}"
         `shouldParse` "{\"key\":[[1,2,3],[4,5,6]]}"
+
   describe "Valid JSON" $
     it "Should validate a JSON with an array leading space" $
       M.parse jsonParser "" "{\"key\":    [\"one\",\"two\"]}"
         `shouldParse` "{\"key\":[\"one\",\"two\"]}"
+
   describe "Valid JSON" $
     it "Should validate a JSON with an array trailing space" $
       M.parse jsonParser "" "{\"key\":[\"one\",\"two\"]    }"
         `shouldParse` "{\"key\":[\"one\",\"two\"]}"
+
   describe "Valid JSON" $
     it "Should validate a JSON with an array leading and trailing space" $
       M.parse jsonParser "" "{\"key\":    [\"one\",\"two\"]    }"
         `shouldParse` "{\"key\":[\"one\",\"two\"]}"
+
   describe "Valid JSON" $
     it "Should validate a JSON with an array space between elements" $
       M.parse jsonParser "" "{\"key\":[\"one\"  ,  \"two\"]}"
         `shouldParse` "{\"key\":[\"one\",\"two\"]}"
+
   describe "Invalid JSON" $
     it "Should fail validation with missing comma" $
       M.parse jsonParser ""
         `shouldFailOn` "{\"key\":[\"one\"\"two\"]}"
+
   describe "Invalid JSON" $
     it "Should fail validation with missing open bracket" $
       M.parse jsonParser ""
         `shouldFailOn` "{\"key\":\"one\",\"two\"]}"
+
   describe "Invalid JSON" $
     it "Should fail validation with missing close bracket" $
       M.parse jsonParser ""
         `shouldFailOn` "{\"key\":[\"one\",\"two\"}"
+
   describe "Invalid JSON" $
     it "Should fail validation with missing both brackets" $
       M.parse jsonParser ""
--- test/ValidateStringSpec.hs
+++ test/ValidateStringSpec.hs
@@ -3,43 +3,51 @@ module ValidateStringSpec (spec) where
 import Test.Hspec
 import Test.Hspec.Megaparsec
 import qualified Text.Megaparsec as M
-import ValidateString (stringParser)
+import Validate (jsonParser)
 
 spec :: Spec
 spec = do
   describe "Valid JSON" $
     it "Should validate with a string value" $
-      M.parse stringParser "" "\"value\""
-        `shouldParse` "\"value\""
+      M.parse jsonParser "" "{\"key\":\"value\"}"
+        `shouldParse` "{\"key\":\"value\"}"
+
   describe "Valid JSON" $
     it "Should validate with an escaped forward slash" $
-      M.parse stringParser "" "\"val\\/ue\""
-        `shouldParse` "\"val\\/ue\""
+      M.parse jsonParser "" "{\"key\":\"val\\/ue\"}"
+        `shouldParse` "{\"key\":\"val\\/ue\"}"
+
   describe "Valid JSON" $
     it "Should validate with an escaped back slash" $
-      M.parse stringParser "" "\"val\\\\ue\""
-        `shouldParse` "\"val\\\\ue\""
+      M.parse jsonParser "" "{\"key\":\"val\\\\ue\"}"
+        `shouldParse` "{\"key\":\"val\\\\ue\"}"
+
   describe "Valid JSON" $
     it "Should validate with an escaped double quote" $
-      M.parse stringParser "" "\"val\\\"ue\""
-        `shouldParse` "\"val\\\"ue\""
+      M.parse jsonParser "" "{\"key\":\"val\\\"ue\"}"
+        `shouldParse` "{\"key\":\"val\\\"ue\"}"
+
   describe "Inalid JSON" $
     it "Should fail validation when missing opening quote" $
-      M.parse stringParser ""
-        `shouldFailOn` "value\""
+      M.parse jsonParser ""
+        `shouldFailOn` "{\"key\":value\"}"
+
   describe "Inalid JSON" $
     it "Should fail validation when missing closing quote" $
-      M.parse stringParser ""
-        `shouldFailOn` "\"value"
+      M.parse jsonParser ""
+        `shouldFailOn` "{\"key\":\"value}"
+
   describe "Inalid JSON" $
     it "Should fail validation with unescaped forward slash" $
-      M.parse stringParser ""
-        `shouldFailOn` "\"val/ue\""
+      M.parse jsonParser ""
+        `shouldFailOn` "{\"key\":\"val/ue\"}"
+
   describe "Inalid JSON" $
     it "Should fail validation with unescaped back slash" $
-      M.parse stringParser ""
-        `shouldFailOn` "\"val\\ue\""
---  describe "Inalid JSON" $
---    it "Should fail validation with unescaped double quote" $
---      M.parse stringParser ""
---        `shouldFailOn` "\"va\"ue\""
+      M.parse jsonParser ""
+        `shouldFailOn` "{\"key\":\"val\\ue\"}"
+
+  describe "Inalid JSON" $
+    it "Should fail validation with unescaped double quote" $
+      M.parse jsonParser ""
+        `shouldFailOn` "{\"key\":\"va\"ue\"}"
