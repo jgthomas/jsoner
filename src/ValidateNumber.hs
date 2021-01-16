ea71e2105629fe4c0ee4321670ab9f966fc7d253
James Thomas <codeactor@protonmail.com>
2021-01-16 12:16:49 +0000

    Move hex validation into own module

--- src/ValidateNumber.hs
+++ src/ValidateNumber.hs
@@ -1,10 +1,10 @@
 module ValidateNumber (numberParser) where
 
-import Data.Char (toLower)
 import Parser (Parser)
 import Text.Megaparsec ((<|>))
 import qualified Text.Megaparsec as M
 import qualified Text.Megaparsec.Char as M
+import ValidateHex (hexParser)
 
 -- | Parse a number value
 numberParser :: Parser String
@@ -33,14 +33,6 @@ integerParser = M.try $ negativeIntParser <|> positiveIntegerParser
     negativeIntParser = M.string "-0" <|> negNumberParser
     positiveIntegerParser = M.string "0" <|> posNumParser
 
-hexParser :: Parser String
-hexParser = do
-  startQuote <- M.char '\"'
-  escape <- M.string "\\u"
-  number <- M.some (hexLetterDigit <|> M.digitChar)
-  endQuote <- M.char '\"'
-  pure $ [startQuote] <> escape <> number <> [endQuote]
-
 posNumParser :: Parser String
 posNumParser = do
   first <- positiveDigit
@@ -56,12 +48,5 @@ negNumberParser = do
 positiveDigit :: Parser Char
 positiveDigit = M.oneOf ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-hexLetterDigit :: Parser Char
-hexLetterDigit = M.satisfy isHexLetterDigit
-
-
 signParser :: Parser Char
 signParser = M.oneOf ['-', '+']
-
-isHexLetterDigit :: Char -> Bool
-isHexLetterDigit c = toLower c `elem` ['a', 'b', 'c', 'd', 'e', 'f']
